e3ba0376cda01d944051f3349275ec323c675d94
Matvey2009 <matvey2009ua@gmail.com>
2021-01-04 21:40:43 +0300

    Визуальная часть

--- modules/witget.py
+++ modules/witget.py
@@ -7,5 +7,10 @@ def start():
     @eel.expose
     def call_in_js(msg):
         print(msg)
+        call_in_python(msg)
+
+    # Отправка сообщения на сайт
+    def call_in_python(msg):
+        eel.call_in_python(msg)
 
     eel.start("index.html", mode="chrome", size = (640, 400))
