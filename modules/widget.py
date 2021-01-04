9a4ff9f66d001e2f2ff65b7ae64c4e911111b5af
Nikita-2007 <nikita2007ua@gmail.com>
2021-01-04 21:25:03 +0300

    дизайн1

--- modules/widget.py
+++ modules/widget.py
@@ -6,6 +6,10 @@ def start():
     #Приём сообщения с сайта
     @eel.expose
     def call_in_js(msg):
-        print(msg)
+        call_in_python(msg)
 
-    eel.start("index.html", mode="chrome", size=(800, 533))
+    #Отправка сообщения из пайтон в сайт
+    def call_in_python(msg):
+        eel.call_in_python(msg)
+
+    eel.start("index.html", mode="chrome", size=(1300, 1000))
